47de391021c75ec69a43f8fd6d719e6b270115b3
Shravani rao <66406267+krao301@users.noreply.github.com>
2022-01-20 23:21:02 +0530

    folder added

---
+++ Online_grocery _store_project/shop/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("admin1/", views.admin1, name="admin1"),
+    path("viewitems/", views.viewitems, name="viewitems"),
+    path("additems/", views.additems, name="additems"),
+    path("adminpage/", views.adminpage, name="adminpage"),
+    path("index/", views.index, name="index"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("update_item/", views.updateItem, name="update_item"),
+    path("product_view/<int:myid>/", views.product_view, name="product_view"),
+    path("search/", views.search, name="search"),
+    path("tracker/", views.tracker, name="tracker"),
+    path("contact/", views.contact, name="contact"),
+    path("loggedin_contact/", views.loggedin_contact, name="loggedin_contact"),
+
+    path("register/", views.register, name="register"),
+    path("change_password/", views.change_password, name="change_password"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
+]
